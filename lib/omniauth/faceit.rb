04de01f916548a0ed593cd0f38ccddd51e187032
Evgeniy <hate2me@yandex.ru>
2019-06-13 13:15:04 +0300

    init

---
+++ lib/omniauth/faceit.rb
@@ -0,0 +1,8 @@
+require "omniauth/faceit/version"
+
+module Omniauth
+  module Faceit
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/faceit/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Faceit
+    VERSION = "0.1.0"
+  end
+end
